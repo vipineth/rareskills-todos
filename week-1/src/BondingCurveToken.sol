1b1c204f9ca9113bf8d20b31e5bb88ba93eb3ccf
vipin <vipineth@gmail.com>
2024-07-02 20:18:09 +0530

    add bondingcurve

--- week-1/src/BondingCurveToken.sol
+++ week-1/src/BondingCurveToken.sol
@@ -1,54 +1,100 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "forge-std/console.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/// @title Bonding Curve Token
+/// @notice A token that follows a bonding curve for minting and redeeming
+/// @dev Implements a bonding curve with a square root function for pricing
 contract BondingCurveToken is ERC20 {
     uint256 public reserveTokenBalance;
+    uint256 constant slope = 10 ** 7;
+
+    event Mint(address indexed to, uint256 amount);
+    event Redeem(address indexed from, uint256 amount);
 
     error InsufficientFunds();
     error ZeroAmountNotAllowed();
     error FailedToSendEther();
     error InsufficientAmount();
 
-    constructor() ERC20("BondingCurveToken", "BCT") {
-        _mint(msg.sender, 1 ether);
+    constructor() ERC20("BondingCurveToken", "BCT") {}
+
+    /// @notice Calculates the square root of a given number
+    /// @param x The number to calculate the square root of
+    /// @return y The square root of the given number
+    function sqrt(uint256 x) internal pure returns (uint256 y) {
+        uint256 z = (x + 1) / 2;
+        y = x;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
     }
 
+    /// @notice Calculates the price to mint a given amount of tokens
+    /// @param amount The amount of tokens to mint
+    /// @return The price in ether to mint the given amount of tokens
     function priceToMint(uint256 amount) public view returns (uint256) {
-        uint256 totalSupply = totalSupply();
-        uint256 price = (totalSupply + amount) ** 2 - totalSupply ** 2;
-        return price;
+        uint256 newTotal = totalSupply() + amount;
+        uint256 newPrice = newTotal ** 2 / (slope);
+        return sqrt(newPrice) - reserveTokenBalance;
     }
 
+    /// @notice Calculates the price to redeem a given amount of tokens
+    /// @param amount The amount of tokens to redeem
+    /// @return The price in ether to redeem the given amount of tokens
     function priceToRedeem(uint256 amount) public view returns (uint256) {
-        uint256 totalSupply = totalSupply();
-        if (amount > totalSupply) revert InsufficientAmount();
-        uint256 price = totalSupply ** 2 - (totalSupply - amount) ** 2;
-        return price;
+        uint256 newTotal = totalSupply() - amount;
+        uint256 newPrice = newTotal ** 2 / (slope);
+        return reserveTokenBalance - sqrt(newPrice);
     }
 
+    /// @notice Mints new tokens by paying the calculated price
+    /// @dev Emits a Mint event
+    /// @param amount The amount of tokens to mint
     function mint(uint256 amount) public payable {
-        if (amount == 0) revert ZeroAmountNotAllowed();
+        if (amount == 0) {
+            revert ZeroAmountNotAllowed();
+        }
 
         uint256 price = priceToMint(amount);
 
-        if (msg.value < price) revert InsufficientFunds();
-        reserveTokenBalance += msg.value;
+        if (msg.value < price) {
+            revert InsufficientFunds();
+        }
+
+        reserveTokenBalance += price;
         _mint(msg.sender, amount);
+        emit Mint(msg.sender, amount);
+        uint256 leftover = msg.value - price;
+
+        if (leftover > 0) {
+            (bool sent, ) = msg.sender.call{value: leftover}("");
+            if (!sent) {
+                revert FailedToSendEther();
+            }
+        }
     }
 
+    /// @notice Redeems tokens for the calculated price
+    /// @dev Emits a Redeem event
+    /// @param amount The amount of tokens to redeem
     function redeem(uint256 amount) public {
-        if (amount == 0) revert ZeroAmountNotAllowed();
-        if (balanceOf(msg.sender) < amount) revert InsufficientAmount();
+        if (amount == 0) {
+            revert ZeroAmountNotAllowed();
+        }
 
-        uint256 price = priceToRedeem(amount);
-        console.log("%s:%s:%s", reserveTokenBalance, price, amount);
-        if (reserveTokenBalance < price) revert InsufficientFunds();
-        _burn(msg.sender, amount);
-        // (bool sent, ) = payable(msg.sender).call{value: price}("");
-        // if (!sent) revert FailedToSendEther();
-        reserveTokenBalance -= price;
+        uint256 returnAmount = priceToRedeem(amount);
+
+        if (returnAmount > 0) {
+            reserveTokenBalance -= returnAmount;
+            _burn(msg.sender, amount);
+            emit Redeem(msg.sender, amount);
+            (bool sent, ) = msg.sender.call{value: returnAmount}("");
+            if (!sent) {
+                revert FailedToSendEther();
+            }
+        }
     }
 }
--- week-1/test/BondingCurveToken.t.sol
+++ week-1/test/BondingCurveToken.t.sol
@@ -2,26 +2,29 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import "forge-std/console.sol";
 import {BondingCurveToken} from "../src/BondingCurveToken.sol";
 
 contract BondingCurveTokenTest is Test {
     BondingCurveToken public bondingCurveToken;
     address deployer;
+    address user1;
+    address user2;
 
     function setUp() public {
         deployer = address(this);
+        user1 = vm.addr(1);
+        user2 = vm.addr(2);
         bondingCurveToken = new BondingCurveToken();
     }
 
     function testMint() public {
-        uint256 mintAmount = 1 ether;
+        uint256 mintAmount = 4.5 ether;
         uint256 expectedPrice = bondingCurveToken.priceToMint(mintAmount);
         vm.deal(deployer, expectedPrice);
         bondingCurveToken.mint{value: expectedPrice}(mintAmount);
         assertEq(
             bondingCurveToken.balanceOf(deployer),
-            2 ether,
+            4.5 ether,
             "Deployer should have 2 ether of tokens after minting"
         );
         assertEq(
@@ -31,40 +34,35 @@ contract BondingCurveTokenTest is Test {
         );
     }
 
-    function testFailMintWithInsufficientFunds() public {
-        uint256 mintAmount = 1 ether;
-        uint256 insufficientFunds = bondingCurveToken.priceToMint(mintAmount) -
-            1;
-        vm.expectRevert(BondingCurveToken.InsufficientFunds.selector);
-        bondingCurveToken.mint{value: insufficientFunds}(mintAmount);
+    function testRedeem() public {
+        uint256 mintAmount = 4.5 ether;
+        uint256 mintPrice = bondingCurveToken.priceToMint(mintAmount);
+        vm.deal(user1, mintPrice);
+        vm.startPrank(user1);
+        bondingCurveToken.mint{value: mintPrice}(mintAmount);
+        uint256 redeemAmount = 2 ether;
+        bondingCurveToken.redeem(redeemAmount);
+        assertEq(
+            bondingCurveToken.balanceOf(user1),
+            2.5 ether,
+            "User1 should have 2.5 ether of tokens after redeeming"
+        );
+        vm.stopPrank();
     }
 
-    function testRedeem() public {
-        uint256 mintAmount = 2 ether;
-        uint256 redeemAmount = 0.5 ether;
-        uint256 mintPrice = bondingCurveToken.priceToMint(mintAmount);
-        vm.deal(deployer, mintPrice);
-        bondingCurveToken.mint{value: mintPrice}(mintAmount);
+    function testMintInsufficientFunds() public {
+        uint256 mintAmount = 10 ether;
+        vm.deal(user1, 10000);
+        vm.startPrank(user1);
+        vm.expectRevert(BondingCurveToken.InsufficientFunds.selector);
+        bondingCurveToken.mint{value: 100}(mintAmount);
+        vm.stopPrank();
+    }
 
-        // uint256 redeemPrice = bondingCurveToken.priceToRedeem(redeemAmount);
-        // console.log(
-        //     "%s:%s:%s",
-        //     bondingCurveToken.totalSupply(),
-        //     bondingCurveToken.reserveTokenBalance(),
-        //     redeemPrice
-        // );
-
-        // console.log(
-        //     "%s:%s",
-        //     bondingCurveToken.balanceOf(deployer),
-        //     redeemAmount
-        // );
-        bondingCurveToken.redeem(redeemAmount);
-
-        assertEq(
-            bondingCurveToken.balanceOf(deployer),
-            2.5 ether,
-            "Deployer should have 2.5 ether of tokens after redeeming"
-        );
+    function testRedeemZero() public {
+        vm.expectRevert(BondingCurveToken.ZeroAmountNotAllowed.selector);
+        vm.startPrank(user1);
+        bondingCurveToken.redeem(0);
+        vm.stopPrank();
     }
 }
