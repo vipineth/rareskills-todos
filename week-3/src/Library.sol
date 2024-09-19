e3a8188c946d60f5064b7f522a72b3a967fb4d3d
vipin <vipineth@gmail.com>
2024-09-19 13:48:19 +0530

    fix

--- week-3/src/Library.sol
+++ week-3/src/Library.sol
@@ -4,13 +4,26 @@ pragma solidity ^0.8.27;
 import {Pair} from "./Pair.sol";
 import {IPair} from "./interfaces/IPair.sol";
 
+/// @title Library for AMM operations
+/// @notice Provides utility functions for token swaps and liquidity calculations
+/// @dev This contract is meant to be used as a library and should not be deployed independently
 contract Library {
+  /// @notice Thrown when the input amount is insufficient for the operation
   error InsufficientInputAmount();
+  /// @notice Thrown when there's not enough liquidity for the operation
   error InsufficientLiquidity();
+  /// @notice Thrown when trying to operate on identical tokens
   error IdenticalToken();
+  /// @notice Thrown when a zero address is provided where a valid address is required
   error ZeroAddress();
+  /// @notice Thrown when the provided path length is invalid
   error InvalidPathLength();
 
+  /// @notice Sorts two token addresses
+  /// @param tokenA The address of the first token
+  /// @param tokenB The address of the second token
+  /// @return token0 The address of the token that comes first in sort order
+  /// @return token1 The address of the token that comes second in sort order
   function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
     if (tokenA == tokenB) revert IdenticalToken();
     if (tokenA == address(0)) revert ZeroAddress();
@@ -18,6 +31,11 @@ contract Library {
     (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
   }
 
+  /// @notice Calculates the address of a pair contract
+  /// @param factory The address of the factory contract
+  /// @param tokenA The address of the first token in the pair
+  /// @param tokenB The address of the second token in the pair
+  /// @return pair The calculated address of the pair contract
   function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
     (address token0, address token1) = sortTokens(tokenA, tokenB);
 
@@ -37,6 +55,12 @@ contract Library {
     );
   }
 
+  /// @notice Fetches the reserves for a pair of tokens
+  /// @param factory The address of the factory contract
+  /// @param tokenA The address of the first token
+  /// @param tokenB The address of the second token
+  /// @return reserveA The reserve of tokenA
+  /// @return reserveB The reserve of tokenB
   function getReserves(address factory, address tokenA, address tokenB)
     internal
     view
@@ -47,12 +71,22 @@ contract Library {
     (reserveA, reserveB) = token0 == tokenA ? (reserve0, reserve1) : (reserve1, reserve0);
   }
 
+  /// @notice Calculates the amount of tokenB equivalent to a given amount of tokenA
+  /// @param amountA The amount of tokenA
+  /// @param reserveA The reserve of tokenA
+  /// @param reserveB The reserve of tokenB
+  /// @return amountB The equivalent amount of tokenB
   function quote(uint256 amountA, uint256 reserveA, uint256 reserveB) internal view returns (uint256 amountB) {
     if (amountA == 0) revert ZeroAmount();
     if (reserveA == 0 || reserveB == 0) revert InsufficientLiquidity();
     amountB = (amountA * reserveB) / reserveA;
   }
 
+  /// @notice Calculates the output amount for a swap
+  /// @param amountIn The input amount
+  /// @param reserveIn The reserve of the input token
+  /// @param reserveOut The reserve of the output token
+  /// @return amountOut The calculated output amount
   function getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut)
     internal
     pure
@@ -73,6 +107,11 @@ contract Library {
     amountOut = numerator / denominator;
   }
 
+  /// @notice Calculates the input amount required for a desired output amount
+  /// @param amountOut The desired output amount
+  /// @param reserveIn The reserve of the input token
+  /// @param reserveOut The reserve of the output token
+  /// @return amountIn The calculated required input amount
   function getAmountIn(uint256 amountOut, uint256 reserveIn, uint256 reserveOut)
     internal
     pure
@@ -96,6 +135,11 @@ contract Library {
   }
 }
 
+/// @notice Calculates the amounts out for a given input amount along a path of token pairs
+/// @param factory The address of the factory contract
+/// @param amountIn The input amount
+/// @param path An array of token addresses representing the path
+/// @return amounts An array of amounts out for each step in the path
 function getAmountsOut(address factory, uint256 amountIn, address[] calldata path)
   internal
   view
@@ -113,6 +157,11 @@ function getAmountsOut(address factory, uint256 amountIn, address[] calldata pat
   }
 }
 
+/// @notice Calculates the amounts in required for a desired output amount along a path of token pairs
+/// @param factory The address of the factory contract
+/// @param amountOut The desired output amount
+/// @param path An array of token addresses representing the path
+/// @return amounts An array of amounts in required for each step in the path
 function getAmountsIn(address factory, uint256 amountOut, address calldata path)
   internal
   view
@@ -124,8 +173,8 @@ function getAmountsIn(address factory, uint256 amountOut, address calldata path)
 
   amounts = new uint256[](path.length);
   amounts[pathLength - 1] = amountOut;
-  for (uint256 i = pathLength - 1; i < 1; i++) {
-    (uint256 reserveIn, address reserveOut) = getReserves(factory, pair[i], pair[i + 1]);
-    amounts[i - 1] = getAmountIn(amountOut, reserveIn, reserveOut);
+  for (uint256 i = pathLength - 1; i > 0; i--) {
+    (uint256 reserveIn, uint256 reserveOut) = getReserves(factory, path[i - 1], path[i]);
+    amounts[i - 1] = getAmountIn(amounts[i], reserveIn, reserveOut);
   }
 }
